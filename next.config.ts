823e7ff91e3f89e16a0d17c123119cd4a1f86ca5
Kranthi Sai <kranthisai@Kranthis-MacBook-Pro.local>
2025-04-15 02:47:10 +0530

    fix: fixes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
+const nextConfig: NextConfig = {
+  output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
